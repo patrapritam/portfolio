982b84c5151af66cea41e0922069a3aa628434c8
Pritam Patra <117983970+patrapritam@users.noreply.github.com>
2025-06-27 00:05:27 +0530

    Update

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { h1 } from "framer-motion/client";
+import react from "react";
+
+const Profile = () => {
+  return(
+    <>
+    <h1>Pritam Patra</h1>
+    
+    
+    </>
+  )
+};
+
+export default Profile;
